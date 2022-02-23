e9433e7fada9c66fffc0ed5266c06ff77927a04b
CaptLWM <wonmin275@gmail.com>
2022-02-23 21:35:58 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { setupSwagger } from 'src/util/setupSwgger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //swagger setup
+  //참고 https://jhyeok.com/nestjs-swagger/
   setupSwagger(app);
   await app.listen(3000);
 }
